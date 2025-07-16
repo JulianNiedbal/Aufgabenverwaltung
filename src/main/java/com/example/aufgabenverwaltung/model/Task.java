55f26cd2ff1d927d22024cfd772e1487a7d8c7a2
JulianNiedbal <Julian.Niedbal@stud.hs-bochum.de>
2025-07-17 01:11:46 +0200

    feat(model): User und Task Model implementiert

---
+++ src/main/java/com/example/aufgabenverwaltung/model/Task.java
@@ -0,0 +1,63 @@
+package com.example.aufgabenverwaltung.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Task {
+
+    @Id
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private boolean completed;
+
+    @Column(name="owner_username")
+    private String ownerUsername;
+
+    public Task() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public String getOwnerUsername() {
+        return ownerUsername;
+    }
+
+    public void setOwnerUsername(String ownerUsername) {
+        this.ownerUsername = ownerUsername;
+    }
+}
---
+++ src/main/java/com/example/aufgabenverwaltung/model/User.java
@@ -0,0 +1,24 @@
+package com.example.aufgabenverwaltung.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String username;
+
+    public User() {}
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
