0a9e0a86c25395034f2eb9c246585701e63f584d
JulianNiedbal <Julian.Niedbal@stud.hs-bochum.de>
2025-07-17 01:10:52 +0200

    initial commit

---
+++ src/test/java/com/example/aufgabenverwaltung/AufgabenverwaltungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aufgabenverwaltung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AufgabenverwaltungApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
