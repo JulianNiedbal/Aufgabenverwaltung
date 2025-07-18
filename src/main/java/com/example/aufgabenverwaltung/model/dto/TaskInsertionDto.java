cd8842cc0dffe20cf5ffcc36a341fdb3c2487626
JulianNiedbal <julian.niedbal@stud.hs-bochum.de>
2025-07-19 00:25:27 +0200

    feature(dto): Task Dto implementiert

---
+++ src/main/java/com/example/aufgabenverwaltung/model/dto/TaskInsertionDto.java
@@ -0,0 +1,43 @@
+package com.example.aufgabenverwaltung.model.dto;
+
+public class TaskInsertionDto {
+
+    private String title;
+
+    private String description;
+
+    private boolean completed;
+
+    public TaskInsertionDto() {
+    }
+
+    public TaskInsertionDto(String title, String description, boolean completed) {
+        this.title = title;
+        this.description = description;
+        this.completed = completed;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
