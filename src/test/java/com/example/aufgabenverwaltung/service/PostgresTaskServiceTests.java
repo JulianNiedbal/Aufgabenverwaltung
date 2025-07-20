d9461914d9d0c950ab94c68bf973884d7953c40d
JulianNiedbal <julian.niedbal@stud.hs-bochum.de>
2025-07-20 23:58:31 +0200

    fix(Profil): postgres Profil für Testcases aktiviert

--- src/test/java/com/example/aufgabenverwaltung/service/PostgresTaskServiceTests.java
+++ src/test/java/com/example/aufgabenverwaltung/service/PostgresTaskServiceTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,10 +14,11 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@ActiveProfiles("postgres")
 public class PostgresTaskServiceTests {
 
     @Autowired
-    TaskService taskService;
+    PostgresTaskService taskService;
 
     @AfterEach
     void tearDown() {
